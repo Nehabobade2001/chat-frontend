e741700db09e10c77c630f4c9c3c979c3851ca0f
Nehabobade2001 <118434258+Nehabobade2001@users.noreply.github.com>
2025-05-20 23:55:21 +0530

    frontend done

--- src/pages/Homedata.tsx
+++ src/pages/Homedata.tsx
@@ -154,7 +154,7 @@ const Homedata: React.FC = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/gemini/generate",
+        "https://sharma-chat-bot-production.up.railway.app",
         { prompt }
       );
       const rawText =
